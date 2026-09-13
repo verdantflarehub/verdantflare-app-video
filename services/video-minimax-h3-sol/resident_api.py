ba6171b3c5649ccf1c5313737e8fc0a4a174acd2
shucheng <shucheng@bd-apaas.com>
2026-09-13 13:49:40 +0800

    fix(h3-sol): align runtime health version

--- services/video-minimax-h3-sol/resident_api.py
+++ services/video-minimax-h3-sol/resident_api.py
@@ -13,7 +13,7 @@ import uuid
 from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
 from urllib.parse import urlsplit
 
-VERSION = 'video-minimax-h3-sol-v0.2.6'
+VERSION = 'video-minimax-h3-sol-v0.2.9'
 ID = re.compile(r'sol_[0-9a-f]{32}$')
 SHA = re.compile(r'[0-9a-f]{64}$')
 
